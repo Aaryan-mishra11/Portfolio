a0a8e1a1f84d093b26e289dfc306551824fb72f7
Aaryan-mishra11 <113164713+Aaryan-mishra11@users.noreply.github.com>
2023-06-12 23:18:09 +0530

    Update javas.js

--- javas.js
+++ javas.js
@@ -37,6 +37,20 @@
         
     };
 
+//======================Scroll reveal===========
+ScrollReveal({ 
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+ });
+
+ ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ ScrollReveal().reveal('.home-img, .services-container , .portfolio-box, .contact form', {origin: 'bottom'});
+ ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+
+
+//===========typed js===============
 
 
 
